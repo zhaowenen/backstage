286414c56801bc2b56fd65d1af5649ee44562f23
renjing <renjing@baojia>
2018-05-23 09:57:19 +0800

    update

--- backstage-api/src/main/java/com/baojia/backstage/api/controller/deposit/DepositController.java
+++ backstage-api/src/main/java/com/baojia/backstage/api/controller/deposit/DepositController.java
@@ -6,13 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.baojia.backstage.api.controller.sys.AbstractController;
 
 /**
- * @author renjing
- * @Title: DepositController
- * @Description: 押金操作信息
- * @date 2018/5/23 08:59
+* 
+* @Title: DepositController  
+* @Description:  押金操作信息
+* @author renjing  
+* @date 2018年5月23日 上午9:40:10
  */
 @RestController
 @RequestMapping("/deposits")
 public class DepositController extends AbstractController {
-	
+	   
 }
