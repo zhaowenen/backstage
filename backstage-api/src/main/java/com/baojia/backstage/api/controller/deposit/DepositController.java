749466b803566b51a13ba00d471889c4505d66b7
renjing <renjing@baojia>
2018-05-23 09:01:23 +0800

    添加押金api

---
+++ backstage-api/src/main/java/com/baojia/backstage/api/controller/deposit/DepositController.java
@@ -0,0 +1,16 @@
+package com.baojia.backstage.api.controller.deposit;
+
+import com.baojia.backstage.api.controller.sys.AbstractController;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author renjing
+ * @Title: DepositController
+ * @Description: 押金操作信息
+ * @date 2018/5/23 08:59
+ */
+@RestController
+@RequestMapping("/deposits")
+public class DepositController extends AbstractController {
+	
+}
